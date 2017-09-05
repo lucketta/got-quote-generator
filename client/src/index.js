7356dd6408d35f88d0e96484234cd73f2fce54d2
lucketta <lucketta@umich.edu>
2017-09-05 03:08:19 -0400

    moved styles into their own folder. Cleaned up index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import './styles/index.css';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import {createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import './index.css';
-import App from './components/App';
 import gotReducer from './reducers/gotReducer'
-
+import thunk from 'redux-thunk'
+import App from './components/App';
 
 const store = createStore(
   gotReducer,
